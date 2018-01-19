2b690a4bb3704f9b94ead72391f46b0d602d1687
itsmeava <immm.ava@gmail.com>
2018-01-19 17:13:20 -0500

    add code to extract script

--- process_text.py
+++ process_text.py
@@ -6,27 +6,11 @@ def scrape_article():
     for article in os.listdir('articles'):
         with open('articles/' + article, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
-            paras = soup.find_all('p', 'story-body-text', text=True)
-            # text = [p.string.encode('utf-8') if p.string is not None else '' for p in paras]
-            # tex t = paras.get_text()
+            paras = soup.find_all('p', 'story-body-text')
 
             text = '\n'.join([para.get_text() for para in paras])
-            print(text)
 
-
-
-            with open('articles_parsed/{}.txt'.format(article), 'a') as new_file:
+            with open('articles_parsed/{}.txt'.format(article), 'w') as new_file:
                 new_file.write(text)
-
-
-        # text = os.path.splitext(article)
-        # soup = BeautifulSoup(text, 'html.parser')
-
-        # paras = soup.find_all('p', 'story-body-text')
-        # text = [p.string.encode('utf-8') if p.string is not None else '' for p in paras]
-
-        
-        # with open('articles_parsed/articles.txt', 'a') as f:
-        #     f.write('\n'.join(text))
 
 scrape_article()
