2a063f938a3e336746d1d145f00c8f6058ae9288
itsmeava <immm.ava@gmail.com>
2018-01-22 15:28:26 -0500

    remove newsletter tags

--- process_text.py
+++ process_text.py
@@ -6,21 +6,29 @@ def scrape_article():
     for article in os.listdir('articles'):
         with open('articles/' + article, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
-            for script in soup(['script', 'style']):
+
+            for script in soup(['script', 'style', 'form', 'meta']):
                 script.decompose()
 
-            for ad in soup.find_all('div', class_='accessibility-ad-header'):
+            # for text in soup(['div', 'a']):
+            #     if class = 'accessibility-ad-header' 'skip-to-text-link'
+
+            for ad in soup.find_all('div', class_=['accessibility-ad-header', 'newsletter-signup']):
                 ad.decompose()
 
             for skip_link in soup.find_all('a', class_='skip-to-text-link'):
                 skip_link.decompose()
 
+            for list_tag in soup.find_all('ul', class_='footer'):
+                list_tag.decompose()
+
             paras = soup.find_all('div', class_="story-body")
 
-            text = '\n'.join([str(para) for para in paras])
+            text = '\n'.join([str(para.prettify()) for para in paras])
 
             with open('articles_parsed_with_html/{}'.format(article), 'w') as new_file:
                 new_file.write(text)
+
 
 if __name__ == '__main__':
     scrape_article()
