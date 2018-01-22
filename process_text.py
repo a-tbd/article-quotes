f57cf7c229bc0041939fe9e51a5d7b470e94a65e
Pedro Baumann <ondoheer@gmail.com>
2018-01-22 15:56:06 -0500

    comments

--- process_text.py
+++ process_text.py
@@ -2,26 +2,35 @@ import os
 from bs4 import BeautifulSoup
 
 
-def scrape_article():
+def scrape_articles():
+    """
+    Parse all the downloaded articles from the articles folder
+    Remove the useless tags and then save that clean html to the articles_parsed_with_thml folder.
+    """
     for article in os.listdir('articles'):
         with open('articles/' + article, 'r') as f:
             soup = BeautifulSoup(f, 'html.parser')
 
+            # Remove these useless tags
             for script in soup(['script', 'style', 'form', 'meta']):
                 script.decompose()
 
             # for text in soup(['div', 'a']):
             #     if class = 'accessibility-ad-header' 'skip-to-text-link'
 
+            # remove the ad tags
             for ad in soup.find_all('div', class_=['accessibility-ad-header', 'newsletter-signup']):
                 ad.decompose()
 
+            # remove the useless anchor tag
             for skip_link in soup.find_all('a', class_='skip-to-text-link'):
-                skip_link.decompose()
+                skip_link.decompose()   
 
+            # Remove the list of related articles
             for list_tag in soup.find_all('ul', class_='footer'):
                 list_tag.decompose()
 
+            # Parse the real story data and send it to new files
             paras = soup.find_all('div', class_="story-body")
 
             text = '\n'.join([str(para.prettify()) for para in paras])
@@ -31,4 +40,4 @@ def scrape_article():
 
 
 if __name__ == '__main__':
-    scrape_article()
+    scrape_articles()
